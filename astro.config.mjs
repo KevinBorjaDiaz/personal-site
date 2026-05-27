45d410d57c0f21c35d2f70c3129cad1ad43a8c1c
Kevin Borja <kevinborjadiaz@hotmail.com>
2026-05-27 13:47:39 -0700

    setting yalm for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 const isDev = process.env.NODE_ENV === 'development';
 
 export default defineConfig({
   site: 'https://kevinborja.github.io',
-  base: isDev ? '/' : process.env.PUBLIC_BASE,
-  vite: {
-    build: {
-      sourcemap: true
-    }
-  }
+  base: isDev ? '/' : "/personal-site",
+
+  integrations: [sitemap()],
 });
